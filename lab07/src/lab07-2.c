32e8e8febebb582b06ae78a6ac0d4d13c853b930
Uladislau-Kulikou <80119117+Uladislau-Kulikou@users.noreply.github.com>
2022-11-27 15:36:19 +0100

    code improvement

--- lab07/src/lab07-2.c
+++ lab07/src/lab07-2.c
@@ -3,48 +3,41 @@
 #include <math.h>
 
 // Повертає мінор за елементом матриці
-int *minor(int item, int size, int *array, int *min) {
-  for (int y = 0; y < size - 1; y++) {
-    for (int x = 0, j = 0; x < size; x++, j++) {
-      if (x != item) {
-        *(min + y * (size - 1) + j) = *(array + (y + 1) * size + x);
-
-      } else {
+void minor(int item, int size, int matrix[][size], int min[][size-1]){
+  for (int y = 0; y < size - 1; y++) 
+    for (int x = 0, j = 0; x < size; x++, j++){
+      min[y][j] = matrix[y+1][x];
+      if (x == item)
         j--;
-      }
-    }
-  }
-  return min;
+    } 
 }
 
 // Приймає необмежено велику матрицю та шляхом розбиття її на матриці 2х2, рахує детермінант
-int determinant(int size, int *array) {
+int determinant(int size, int matrix[][size]) {
   int det = 0;
 
   if (size == 1){
-    return *array;
+    return matrix[0][0];
   }
   if (size == 2){
-    return (*(array + 0 * size + 0) * *(array + 1 * size + 1) -
-            *(array + 0 * size + 1) * *(array + 1 * size + 0));
-  }
+    return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1];
+  }           
   for (int k = 0; k < size; k++) {
     int next_matrix[size - 1][size - 1];
-    det += *(array + 0 * size + k) * pow(-1, k) * determinant(size - 1, minor(k, size, array, *next_matrix));
+    minor(k, size, matrix, next_matrix);
+    det += matrix[0][k] * pow(-1, k) * determinant(size-1, next_matrix);
   }
-  
   return det;
 }
 
 int main() {
+  int matrix[N][N] = {{1, 1, 1, 1, 1},
+                      {1, 2, 2, 2, 2},
+                      {1, 2, 3, 3, 3},
+                      {1, 2, 3, 4, 4},
+                      {1, 2, 3, 4, 9}}; // Вхідні дані
 
-  int nums[N][N] = {{1, 1, 1, 1, 1},
-                    {1, 2, 2, 2, 2},
-                    {1, 2, 3, 3, 3},
-                    {1, 2, 3, 4, 4},
-                    {1, 2, 3, 4, 9}}; // Вхідні дані
-
-  int result = determinant(N, *nums); // Результат виконання
+  int result = determinant(N, matrix); // Результат виконання
 
   return 0;
 }
