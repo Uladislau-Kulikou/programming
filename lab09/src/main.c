a96795a5e0f81a3155c3b145540b5a03d6bbfc39
Vlad <kulikovvlad186@gmail.com>
2023-03-15 15:13:51 +0100

    added lab09

---
+++ lab09/src/main.c
@@ -0,0 +1,14 @@
+#include "det.c"
+#include <check.h>
+#define N 3
+
+int main(){
+    /// Вхідні дані
+    int matrix[N][N] = {{1, 2, 9},
+                        {4, 5, 6},
+                        {7, 8, 9}}; 
+
+    /// Результат виконання
+    int result = determinant(N, matrix); 
+    return 0;
+}
